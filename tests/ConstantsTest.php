80adc2f4a1332c45cd6a07713c211d6d15103ed3
Guido Contreras Woda <guiwoda@gmail.com>
2016-01-31 18:52:02 -0300

    Initial commit

---
+++ tests/ConstantsTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace TimeZone;
+
+class ConstantsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider timezones
+     */
+    public function it_should_contain_the_upper_cased_constant_version_of_the_timezone_string($timezone)
+    {
+        $constant = strtoupper(preg_replace('/[^a-zA-Z0-9]/', '_', $timezone));
+
+        $fullyQualifiedConstant = "TimeZone\\Constants::$constant";
+
+        $this->assertTrue(defined($fullyQualifiedConstant), "Constant $constant is not defined.");
+        $this->assertEquals(constant($fullyQualifiedConstant), $timezone, "Constant $constant and $timezone differ.");
+    }
+
+    public function timezones()
+    {
+        return array_map(function($timezone){
+            return [$timezone];
+        }, \DateTimeZone::listIdentifiers());
+    }
+}
